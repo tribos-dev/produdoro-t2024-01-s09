2a128ad989549f7058f99dac04d4ecf67053497c
Ariane Pinheiro <arianepinheirodev@gmail.com>
2024-09-21 22:32:07 -0300

    feat: implementa camada controller

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationService.java
@@ -40,4 +40,9 @@ public class TarefaApplicationService implements TarefaService {
         log.info("[finaliza] TarefaApplicationService - detalhaTarefa");
         return tarefa;
     }
+
+    @Override
+    public void ativaTarefa(String usuarioEmail, UUID idTarefa) {
+        
+    }
 }
