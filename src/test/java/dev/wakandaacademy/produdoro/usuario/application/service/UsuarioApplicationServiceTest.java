68546f753033cc1d99bd32affaf051f7e71c5194
RamonBarbosa <ramonfernandesbarbosa13@gmail.com>
2024-09-23 04:52:11 -0300

    test: implementa cenario negativo

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,6 +1,7 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
 import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
 import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
 import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -35,5 +39,16 @@ class UsuarioApplicationServiceTest {
 
             verify(usuarioRepository, times(1)).salva(usuario);
             assertEquals(StatusUsuario.PAUSA_CURTA, usuario.getStatus());
+        }
+
+        @Test
+        void naoDeveAlterarStatusParaPausaCurta() {
+
+            Usuario usuario = DataHelper.createUsuario();
+            UUID idUsuario = UUID.fromString("f81d4fae-7dec-4f34-a5e2-8c9559d13911");
+            when(usuarioRepository.buscaUsuarioPorEmail((anyString()))).thenReturn(usuario);
+            APIException e = assertThrows(APIException.class,
+                    () -> usuarioApplicationService.mudaStatusParaPausaCurta(usuario.getEmail(), idUsuario));
+            assertEquals(HttpStatus.UNAUTHORIZED, e.getStatusException());
         }
 }
