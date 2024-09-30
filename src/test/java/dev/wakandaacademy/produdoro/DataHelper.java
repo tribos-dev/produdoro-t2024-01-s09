ad5ad11a0aabb90d2151387b083e65cd0616843d
pedhros <pedrooohsss@gmail.com>
2024-09-30 20:34:26 -0300

    implementa testes unitários

--- src/test/java/dev/wakandaacademy/produdoro/DataHelper.java
+++ src/test/java/dev/wakandaacademy/produdoro/DataHelper.java
@@ -7,6 +7,7 @@ import dev.wakandaacademy.produdoro.pomodoro.domain.ConfiguracaoPadrao;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaEditaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
 import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
@@ -14,61 +15,72 @@ import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 public class DataHelper {
 
-    private static final UUID usuario1 = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f4");
+	private static final UUID usuario1 = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f4");
 
-    private static final UUID usuarioFOCO = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f2");
+	private static final UUID usuarioFOCO = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f2");
 
-    public static Usuario createUsuario() {
-        return Usuario.builder().email("email@email.com").status(StatusUsuario.PAUSA_LONGA).idUsuario(usuario1).build();
-    }
+	public static Usuario createUsuario() {
+		return Usuario.builder().email("email@email.com").status(StatusUsuario.PAUSA_LONGA).idUsuario(usuario1).build();
+	}
 
-    public static Tarefa createTarefa() {
-        return Tarefa.builder().contagemPomodoro(1).idTarefa(UUID.fromString("06fb5521-9d5a-461a-82fb-e67e3bedc6eb"))
-                .idUsuario(usuario1).descricao("descricao tarefa").statusAtivacao(StatusAtivacaoTarefa.INATIVA).build();
-    }
+	public static Tarefa createTarefa() {
+		return Tarefa.builder().contagemPomodoro(1).idTarefa(UUID.fromString("06fb5521-9d5a-461a-82fb-e67e3bedc6eb"))
+				.idUsuario(usuario1).descricao("descricao tarefa").statusAtivacao(StatusAtivacaoTarefa.INATIVA).build();
+	}
 
-    public static UsuarioNovoRequest getUsuarioRequest() {
-        UsuarioNovoRequest userReq = new UsuarioNovoRequest("email@email.com", "12345");
-        return userReq;
-    }
+	public static UsuarioNovoRequest getUsuarioRequest() {
+		UsuarioNovoRequest userReq = new UsuarioNovoRequest("email@email.com", "12345");
+		return userReq;
+	}
 
-    public static ConfiguracaoPadrao getConfig() {
-        return ConfiguracaoPadrao.builder().tempoMinutosFoco(25).tempoMinutosPausaCurta(5).tempoMinutosPausaLonga(15)
-                .repeticoesParaPausaLonga(3).build();
-    }
+	public static ConfiguracaoPadrao getConfig() {
+		return ConfiguracaoPadrao.builder().tempoMinutosFoco(25).tempoMinutosPausaCurta(5).tempoMinutosPausaLonga(15)
+				.repeticoesParaPausaLonga(3).build();
+	}
 
-    public static TarefaRequest getTarefaRequest() {
-        Usuario usuario = createUsuario();
+	public static TarefaRequest getTarefaRequest() {
+		Usuario usuario = createUsuario();
 
-        TarefaRequest tarefaReq = new TarefaRequest("descricao", usuario.getIdUsuario(), null, null, 1);
-        return tarefaReq;
-    }
+		TarefaRequest tarefaReq = new TarefaRequest("descricao", usuario.getIdUsuario(), null, null, 1);
+		return tarefaReq;
+	}
 
-    public static List<Tarefa> createListTarefa() {
-        return List.of(Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 1").idUsuario(usuario1).build(),
-                Tarefa.builder().build(),
-                Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 2").idUsuario(usuario1).build(),
-                Tarefa.builder().build(),
-                Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 3").idUsuario(usuario1).build(),
-                Tarefa.builder().build(),
-                Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 4").idUsuario(usuario1).build(),
-                Tarefa.builder().build()
+	public static List<Tarefa> createListTarefa() {
+		return List.of(Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 1").idUsuario(usuario1).build(),
+				Tarefa.builder().build(),
+				Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 2").idUsuario(usuario1).build(),
+				Tarefa.builder().build(),
+				Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 3").idUsuario(usuario1).build(),
+				Tarefa.builder().build(),
+				Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 4").idUsuario(usuario1).build(),
+				Tarefa.builder().build()
 
-        );
-    }
+		);
+	}
 
-    public static Usuario createUsuarioFOCO() {
-        return Usuario.builder().email("email@email.com").status(StatusUsuario.FOCO).idUsuario(usuarioFOCO).build();
-    }
+	public static Usuario createUsuarioFOCO() {
+		return Usuario.builder().email("email@email.com").status(StatusUsuario.FOCO).idUsuario(usuarioFOCO).build();
+	}
 
-    public static Tarefa createTarefaPorIdUsuario(UUID idUsuario) {
-        return Tarefa.builder().contagemPomodoro(1).idTarefa(UUID.fromString("06fb5521-9d5a-461a-82fb-e67e3bedc6eb"))
-                .idUsuario(usuarioFOCO).descricao("descricao tarefa").statusAtivacao(StatusAtivacaoTarefa.INATIVA).build();
-    }
+	public static Tarefa createTarefaPorIdUsuario(UUID idUsuario) {
+		return Tarefa.builder().contagemPomodoro(1).idTarefa(UUID.fromString("06fb5521-9d5a-461a-82fb-e67e3bedc6eb"))
+				.idUsuario(usuarioFOCO).descricao("descricao tarefa").statusAtivacao(StatusAtivacaoTarefa.INATIVA)
+				.build();
+	}
 
-    public static TarefaEditaRequest getTarefaEditaRequest() {
-        TarefaEditaRequest tarefaEdita = new TarefaEditaRequest("Tarefa de teste");
-        return tarefaEdita;
-    }
+	public static TarefaEditaRequest getTarefaEditaRequest() {
+		TarefaEditaRequest tarefaEdita = new TarefaEditaRequest("Tarefa de teste");
+		return tarefaEdita;
+	}
+
+	public static List<Tarefa> createTarefasConcluidas() {
+		return List.of(
+				Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 1").idUsuario(usuario1)
+						.status(StatusTarefa.CONCLUIDA).build(),
+				Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 2").idUsuario(usuario1)
+						.status(StatusTarefa.CONCLUIDA).build(),
+				Tarefa.builder().idTarefa(UUID.randomUUID()).descricao("tarefa 3").idUsuario(usuario1)
+						.status(StatusTarefa.CONCLUIDA).build());
+	}
 
 }
--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -30,182 +30,212 @@ import static org.mockito.ArgumentMatchers.anyString;
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
 
-    // @Autowired
-    @InjectMocks
-    TarefaApplicationService tarefaApplicationService;
+	// @Autowired
+	@InjectMocks
+	TarefaApplicationService tarefaApplicationService;
 
-    // @MockBean
-    @Mock
-    TarefaRepository tarefaRepository;
-    @Mock
-    UsuarioRepository usuarioRepository;
+	// @MockBean
+	@Mock
+	TarefaRepository tarefaRepository;
+	@Mock
+	UsuarioRepository usuarioRepository;
 
-    @Test
-    void deveRetornarIdTarefaNovaCriada() {
-        TarefaRequest request = getTarefaRequest();
-        when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request));
+	@Test
+	void deveRetornarIdTarefaNovaCriada() {
+		TarefaRequest request = getTarefaRequest();
+		when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request, 0));
 
-        TarefaIdResponse response = tarefaApplicationService.criaNovaTarefa(request);
+		TarefaIdResponse response = tarefaApplicationService.criaNovaTarefa(request);
 
-        assertNotNull(response);
-        assertEquals(TarefaIdResponse.class, response.getClass());
-        assertEquals(UUID.class, response.getIdTarefa().getClass());
-    }
+		assertNotNull(response);
+		assertEquals(TarefaIdResponse.class, response.getClass());
+		assertEquals(UUID.class, response.getIdTarefa().getClass());
+	}
 
-    @Test
-    void deveRetornarTarefaAtiva() {
-        Tarefa tarefa = DataHelper.createTarefa();
-        Usuario usuario = DataHelper.createUsuario();
-        when(usuarioRepository.buscaUsuarioPorEmail(usuario.getEmail())).thenReturn(usuario);
-        when(tarefaRepository.buscaTarefaPorId(tarefa.getIdTarefa())).thenReturn(Optional.of(tarefa));
-        tarefaApplicationService.ativaTarefa(usuario.getEmail(), tarefa.getIdTarefa());
-        verify(tarefaRepository).ativaTarefaUsuario(tarefa);
-        verify(tarefaRepository, times(1)).salva(tarefa);
-    }
+	@Test
+	void deveRetornarTarefaAtiva() {
+		Tarefa tarefa = DataHelper.createTarefa();
+		Usuario usuario = DataHelper.createUsuario();
+		when(usuarioRepository.buscaUsuarioPorEmail(usuario.getEmail())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorId(tarefa.getIdTarefa())).thenReturn(Optional.of(tarefa));
+		tarefaApplicationService.ativaTarefa(usuario.getEmail(), tarefa.getIdTarefa());
+		verify(tarefaRepository).ativaTarefaUsuario(tarefa);
+		verify(tarefaRepository, times(1)).salva(tarefa);
+	}
 
-    @Test
-    void deveDeletarTodasTarefasDoUsuario() {
-        Usuario usuario = DataHelper.createUsuario();
-        List<Tarefa> tarefas = DataHelper.createListTarefa();
-        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
-        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
-        when(tarefaRepository.buscaTarefaPorUsuario(any())).thenReturn(tarefas);
-        tarefaApplicationService.deletaTodasTarefas(usuario.getEmail(), usuario.getIdUsuario());
-        verify(tarefaRepository, times(1)).deletaTodasTarefas(tarefas);
-    }
+	@Test
+	void deveDeletarTodasTarefasDoUsuario() {
+		Usuario usuario = DataHelper.createUsuario();
+		List<Tarefa> tarefas = DataHelper.createListTarefa();
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorUsuario(any())).thenReturn(tarefas);
+		tarefaApplicationService.deletaTodasTarefas(usuario.getEmail(), usuario.getIdUsuario());
+		verify(tarefaRepository, times(1)).deletaTodasTarefas(tarefas);
+	}
 
-    @Test
-    void deveListarTodasAsTarefas() {
-        // Dado
-        Usuario usuario = DataHelper.createUsuario();
-        List<Tarefa> tarefas = DataHelper.createListTarefa();
-        // Quando
-        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
-        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
-        when(tarefaRepository.buscaTarefaPorUsuario(any())).thenReturn(tarefas);
+	@Test
+	void deveListarTodasAsTarefas() {
+		// Dado
+		Usuario usuario = DataHelper.createUsuario();
+		List<Tarefa> tarefas = DataHelper.createListTarefa();
+		// Quando
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorUsuario(any())).thenReturn(tarefas);
 
-        List<TarefaListResponse> resultado = tarefaApplicationService.buscaTarefasUsuario(usuario.getEmail(),
-                usuario.getIdUsuario());
+		List<TarefaListResponse> resultado = tarefaApplicationService.buscaTarefasUsuario(usuario.getEmail(),
+				usuario.getIdUsuario());
 
-        // Então
-        verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(usuario.getEmail());
-        verify(usuarioRepository, times(1)).buscaUsuarioPorId(usuario.getIdUsuario());
-        verify(tarefaRepository, times(1)).buscaTarefaPorUsuario(usuario.getIdUsuario());
-        assertEquals(resultado.size(), 8);
-    }
+		// Então
+		verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(usuario.getEmail());
+		verify(usuarioRepository, times(1)).buscaUsuarioPorId(usuario.getIdUsuario());
+		verify(tarefaRepository, times(1)).buscaTarefaPorUsuario(usuario.getIdUsuario());
+		assertEquals(resultado.size(), 8);
+	}
 
-    @Test
-    void naoDeveBuscarTodasTarefasPorUsuario() {
-        Usuario usuario = DataHelper.createUsuario();
-        when(usuarioRepository.buscaUsuarioPorEmail(any()))
-                .thenThrow(APIException.build(HttpStatus.BAD_REQUEST, "Usuario não encontrado!"));
+	@Test
+	void naoDeveBuscarTodasTarefasPorUsuario() {
+		Usuario usuario = DataHelper.createUsuario();
+		when(usuarioRepository.buscaUsuarioPorEmail(any()))
+				.thenThrow(APIException.build(HttpStatus.BAD_REQUEST, "Usuario não encontrado!"));
 
-        APIException e = assertThrows(APIException.class, () -> tarefaApplicationService
-                .buscaTarefasUsuario("emailinvalido@gmail.com", usuario.getIdUsuario()));
+		APIException e = assertThrows(APIException.class,
+				() -> tarefaApplicationService.buscaTarefasUsuario("emailinvalido@gmail.com", usuario.getIdUsuario()));
 
-        assertEquals(HttpStatus.BAD_REQUEST, e.getStatusException());
-        assertEquals("Usuario não encontrado!", e.getMessage());
-    }
+		assertEquals(HttpStatus.BAD_REQUEST, e.getStatusException());
+		assertEquals("Usuario não encontrado!", e.getMessage());
+	}
 
-    public TarefaRequest getTarefaRequest() {
-        TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
-        return request;
-    }
+	public TarefaRequest getTarefaRequest() {
+		TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
+		return request;
+	}
 
-    @Test
-    @DisplayName("Verifica se incrementou pomodoro")
-    void incrementaPomodoroTest() {
-        Usuario usuario = DataHelper.createUsuarioFOCO();
-        Tarefa tarefa = DataHelper.createTarefaPorIdUsuario(usuario.getIdUsuario());
-        int contagemPomodoroAntes = tarefa.getContagemPomodoro();
+	@Test
+	@DisplayName("Verifica se incrementou pomodoro")
+	void incrementaPomodoroTest() {
+		Usuario usuario = DataHelper.createUsuarioFOCO();
+		Tarefa tarefa = DataHelper.createTarefaPorIdUsuario(usuario.getIdUsuario());
+		int contagemPomodoroAntes = tarefa.getContagemPomodoro();
 
-        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
-        when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
-        tarefaApplicationService.incrementaPomodoro(usuario.getEmail(), tarefa.getIdTarefa());
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
+		tarefaApplicationService.incrementaPomodoro(usuario.getEmail(), tarefa.getIdTarefa());
 
-        int contagemPomodoroDepois = tarefa.getContagemPomodoro();
-        verify(tarefaRepository, times(1)).salva(any());
-        assertEquals(contagemPomodoroAntes + 1, contagemPomodoroDepois);
-    }
+		int contagemPomodoroDepois = tarefa.getContagemPomodoro();
+		verify(tarefaRepository, times(1)).salva(any());
+		assertEquals(contagemPomodoroAntes + 1, contagemPomodoroDepois);
+	}
 
-    @Test
-    @DisplayName("incrementaPomodoro não encontra tarefa, NOT_FOUND")
-    void incrementaPomodoroJogaExceptionSeNaoEncontraTarefa() {
-        Usuario usuario = DataHelper.createUsuarioFOCO();
-        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
-        assertThrows(APIException.class, () -> tarefaApplicationService
-                .incrementaPomodoro(usuario.getEmail(), UUID.randomUUID()));
-    }
+	@Test
+	@DisplayName("incrementaPomodoro não encontra tarefa, NOT_FOUND")
+	void incrementaPomodoroJogaExceptionSeNaoEncontraTarefa() {
+		Usuario usuario = DataHelper.createUsuarioFOCO();
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		assertThrows(APIException.class,
+				() -> tarefaApplicationService.incrementaPomodoro(usuario.getEmail(), UUID.randomUUID()));
+	}
 
-    @Test
-    @DisplayName("incrementaPomodoro fornece token nao autorizado, UNAUTHORIZED")
-    void incrementaPomodoroJogaExceptionSeRetornaTokenNaoAutorizado() {
-        Usuario usuario = DataHelper.createUsuario();
-        Usuario usuario2 = DataHelper.createUsuarioFOCO();
-        Tarefa tarefa = DataHelper.createTarefaPorIdUsuario(usuario2.getIdUsuario());
+	@Test
+	@DisplayName("incrementaPomodoro fornece token nao autorizado, UNAUTHORIZED")
+	void incrementaPomodoroJogaExceptionSeRetornaTokenNaoAutorizado() {
+		Usuario usuario = DataHelper.createUsuario();
+		Usuario usuario2 = DataHelper.createUsuarioFOCO();
+		Tarefa tarefa = DataHelper.createTarefaPorIdUsuario(usuario2.getIdUsuario());
 
-        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
-        when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
-        assertThrows(APIException.class,
-                () -> tarefaApplicationService.incrementaPomodoro(usuario.getEmail(), tarefa.getIdTarefa()));
-    }
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
+		assertThrows(APIException.class,
+				() -> tarefaApplicationService.incrementaPomodoro(usuario.getEmail(), tarefa.getIdTarefa()));
+	}
 
-    @Test
-    @DisplayName("Deve editar a tarefa")
-    void deveEditarTarefa() {
-        // Dado
-        Usuario usuario = DataHelper.createUsuario();
-        Tarefa tarefa = DataHelper.createTarefa();
-        TarefaEditaRequest tarefaEdita = DataHelper.getTarefaEditaRequest();
-        // Quando
-        when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
-        when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
-        tarefaApplicationService.editaTarefa(usuario.getEmail(), tarefaEdita, tarefa.getIdTarefa());
-        // Entao
-        verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(usuario.getEmail());
-        verify(tarefaRepository, times(1)).buscaTarefaPorId(tarefa.getIdTarefa());
-        verify(tarefaRepository, times(1)).salva(tarefa);
-        assertEquals("Tarefa de teste", tarefa.getDescricao());
-    }
+	@Test
+	@DisplayName("Deve editar a tarefa")
+	void deveEditarTarefa() {
+		// Dado
+		Usuario usuario = DataHelper.createUsuario();
+		Tarefa tarefa = DataHelper.createTarefa();
+		TarefaEditaRequest tarefaEdita = DataHelper.getTarefaEditaRequest();
+		// Quando
+		when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
+		tarefaApplicationService.editaTarefa(usuario.getEmail(), tarefaEdita, tarefa.getIdTarefa());
+		// Entao
+		verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(usuario.getEmail());
+		verify(tarefaRepository, times(1)).buscaTarefaPorId(tarefa.getIdTarefa());
+		verify(tarefaRepository, times(1)).salva(tarefa);
+		assertEquals("Tarefa de teste", tarefa.getDescricao());
+	}
 
-    @Test
-    @DisplayName("Não deve editar a tarefa")
-    void naoDeveEditarTarefa() {
-        // Dado
-        UUID idTarefaInvalida = UUID.randomUUID();
-        String usuario = "usuarioqualquerum@hotmail.com";
-        TarefaEditaRequest tarefaEdita = DataHelper.getTarefaEditaRequest();
-        // Quando
-        when(tarefaRepository.buscaTarefaPorId(idTarefaInvalida)).thenReturn(Optional.empty());
-        assertThrows(APIException.class,
-                () -> tarefaApplicationService.editaTarefa(usuario, tarefaEdita, idTarefaInvalida));
-        // Entao
-        verify(tarefaRepository, times(1)).buscaTarefaPorId(idTarefaInvalida);
-    }
+	@Test
+	@DisplayName("Não deve editar a tarefa")
+	void naoDeveEditarTarefa() {
+		// Dado
+		UUID idTarefaInvalida = UUID.randomUUID();
+		String usuario = "usuarioqualquerum@hotmail.com";
+		TarefaEditaRequest tarefaEdita = DataHelper.getTarefaEditaRequest();
+		// Quando
+		when(tarefaRepository.buscaTarefaPorId(idTarefaInvalida)).thenReturn(Optional.empty());
+		assertThrows(APIException.class,
+				() -> tarefaApplicationService.editaTarefa(usuario, tarefaEdita, idTarefaInvalida));
+		// Entao
+		verify(tarefaRepository, times(1)).buscaTarefaPorId(idTarefaInvalida);
+	}
 
-    @Test
-    @DisplayName("Deve concluir tarefa")
-    void deveConcluirTarefa() {
-        // Dado
-        Usuario usuario = DataHelper.createUsuario();
-        Tarefa tarefa = DataHelper.createTarefa();
-        // Quando
-        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
-        when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
-        tarefaApplicationService.concluiTarefa(usuario.getEmail(), tarefa.getIdTarefa());
-        // Entao
-        assertEquals(tarefa.getStatus(), StatusTarefa.CONCLUIDA);
-    }
+	@Test
+	@DisplayName("Deve concluir tarefa")
+	void deveConcluirTarefa() {
+		// Dado
+		Usuario usuario = DataHelper.createUsuario();
+		Tarefa tarefa = DataHelper.createTarefa();
+		// Quando
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
+		tarefaApplicationService.concluiTarefa(usuario.getEmail(), tarefa.getIdTarefa());
+		// Entao
+		assertEquals(tarefa.getStatus(), StatusTarefa.CONCLUIDA);
+	}
 
-    @Test
-    @DisplayName("Não deve concluir tarefa")
-    void naoDeveConcluirTarefa() {
-        // Dado
-        Tarefa tarefa = DataHelper.createTarefa();
-        // Quando
-        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenThrow(APIException.class);
-        // Entao
-        assertThrows(APIException.class,
-                () -> tarefaApplicationService.concluiTarefa("emailqualquerum@hotmail.com", tarefa.getIdTarefa()));
-    }
+	@Test
+	@DisplayName("Não deve concluir tarefa")
+	void naoDeveConcluirTarefa() {
+		// Dado
+		Tarefa tarefa = DataHelper.createTarefa();
+		// Quando
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenThrow(APIException.class);
+		// Entao
+		assertThrows(APIException.class,
+				() -> tarefaApplicationService.concluiTarefa("emailqualquerum@hotmail.com", tarefa.getIdTarefa()));
+	}
+
+	@Test
+	@DisplayName("Deleta tarefas concluidas")
+	void deletaTarefasConcluidas_comDadosValidos_sucesso() {
+		Usuario usuario = DataHelper.createUsuario();
+		List<Tarefa> tarefasConcluidas = DataHelper.createTarefasConcluidas();
+		List<Tarefa> tarefas = DataHelper.createListTarefa();
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefasConcluidas(any())).thenReturn(tarefasConcluidas);
+		when(tarefaRepository.buscaTarefaPorUsuario(any())).thenReturn(tarefas);
+
+		tarefaApplicationService.deletaTarefasConcluidas(usuario.getEmail(), usuario.getIdUsuario());
+
+		verify(tarefaRepository, times(1)).deletaVariasTarefas(tarefasConcluidas);
+		verify(tarefaRepository, times(1)).atualizaPosicaoDaTarefa(tarefas);
+	}
+
+	@Test
+	@DisplayName("Deleta tarefas concluidas quando email for inexistente")
+	void deletaTarefasConcluidas_comEmailInexistente_retornaAPIException() {
+		String email = "emailinvalido@gmail.com";
+		when(usuarioRepository.buscaUsuarioPorEmail(any()))
+				.thenThrow(APIException.build(HttpStatus.BAD_REQUEST, "Usuario não encontrado!"));
+
+		assertThrows(APIException.class,
+				() -> tarefaApplicationService.deletaTarefasConcluidas(email, UUID.randomUUID()));
+
+		verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(email);
+	}
 }
